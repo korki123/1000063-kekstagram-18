e3938cbf4cd2ab7f18dc6dc20fc62e0b03213b76
Игорь Хан <korki123@mail.ru>
2019-09-30 00:35:39 +0300

    Начал, но ничего не понял

---
+++ js/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var uploadFile = document.querySelector('#upload-file');
+uploadFile.addEventListener('change', function(ext));
+var onVisuallyOff = uploadFile.classList.remove('visually-hidden');
